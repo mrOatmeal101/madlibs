c087538f5f5d85734acc97eab9d9c42d3e53a26a
mroatmeal <jfeind101@gmail.com>
2024-11-06 11:25:06 -0600

    end of section 1

---
+++ notes.py
@@ -0,0 +1,5 @@
+# Section 1 Flask Jinja Handout
+# You don't always have to respond to HTTP requests with plain text. You can choose to respond to them with HTML. 
+# While we can output a string of HTML, it is better practice to output a separate HTML file. Colt will walk you through this. 
+
+# Section 2 Introduction to Templates
